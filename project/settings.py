58a3fb5b070ab3ae41a9ef883494379f0cc761f3
dev-vikash2v7 <vikashvermacom92@gmail.com>
2025-01-25 17:50:37 +0530

    final

--- project/settings.py
+++ project/settings.py
@@ -16,6 +16,7 @@ django.setup()
 
 
 INSTALLED_APPS = [
+    'chat',
     'daphne',
     'channels',
     'django.contrib.admin',
@@ -24,7 +25,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chat'
 ]
 
 MIDDLEWARE = [
